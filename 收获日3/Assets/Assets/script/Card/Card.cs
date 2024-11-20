6e6d9899d8fbdf4ac9785815f2ca3ffca3d3f24f
Smallsign <1131431962@qq.com>
2024-11-20 17:50:25 +0800

    更新

--- 收获日3/Assets/Assets/script/Card/Card.cs
+++ 收获日3/Assets/Assets/script/Card/Card.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using DG.Tweening;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler, IPointerDownHandler
 {
     public CardData CardData;
     public SpriteRenderer cardImage;
     public TextMeshPro Description;
+    [Header("状态")]
+    public bool isShow;
+    [Header("原始位置")]
+    public Vector3 OriginalPos;
+    public Quaternion OriginalRoation;
+    public int Soring;
+    public Vector3 MovePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +30,56 @@ public class Card : MonoBehaviour
     {
         
     }
+
+    public void SetOriginalData(Vector3 Pos,Quaternion Roa,int i)
+    {
+        OriginalPos= Pos;
+        OriginalRoation = Roa;
+        Soring= i;
+    }
     //初始化
     public void InitializeCard()
     {
         cardImage.sprite = CardData.cardImage;
         Description.text = CardData.cardDescription;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+
+    }
+    public void ResetPos()
+    {
+        transform.DOMove(OriginalPos, 0.1f);
+        transform.DORotateQuaternion(OriginalRoation, 0.1f);
+        GetComponent<SortingGroup>().sortingOrder = Soring;
+        isShow = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isShow)
+        {
+            isShow = true;
+            Vector3 TargetPos = OriginalPos + MovePos;
+            transform.DOMove(TargetPos, 0.1f);
+            transform.DORotate(new Vector3(1, 1, 1), 0.1f);
+            GetComponent<SortingGroup>().sortingOrder = 20;
+        }
+        else
+        {
+            ResetPos();
+        }
     }
 }
